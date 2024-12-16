54d74616f6f9d4e7e82f2d68d123e0a6e4dea0ef
RohitSathya <156504207+RohitSathya@users.noreply.github.com>
2024-12-16 13:30:41 +0530

    Update tailwind.config.js

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -1,6 +1,5 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"], // Scan all files in the `src` directory
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
